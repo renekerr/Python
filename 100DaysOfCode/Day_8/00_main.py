6735e550672c36fd1d29b9e0370f8de1c49ab44f
renekerr <rrenekerr@gmail.com>
2024-02-19 16:52:42 +0100

    updates from work

--- 100DaysOfCode/Day_8/00_main.py
+++ 100DaysOfCode/Day_8/00_main.py
@@ -9,56 +9,56 @@ Day 8 - Functions with inputs (parameters)
                                                                                                                                 
 
 '''
-# # Greeting I
-# def greet():
-#     print('Hi there!')
+# Greeting I
+def greet():
+    print('Hi there!')
 
-# greet()
+greet()
 
-# # Sum numbers
-# def suma():
-#     a = 3
-#     b = 2
-#     print(a+b)
+# Sum numbers
+def suma():
+    a = 3
+    b = 2
+    print(a+b)
 
-# suma()
+suma()
 
-# # Greeting II
-# def greeting_name_location(name, location):
-#     print(f'Hi {name}')
-#     print(f'How is the weather like in {location}?')
+# Greeting II
+def greeting_name_location(name, location):
+    print(f'Hi {name}')
+    print(f'How is the weather like in {location}?')
 
-# greeting_name_location('René', 'Madrid')
+greeting_name_location('René', 'Madrid')
 
-# # Greeting III
-# def greeting_position(name, location):
-#     print(f"Hi {name}")
-#     print(f"What's the weather like in {location}?")
+# Greeting III
+def greeting_position(name, location):
+    print(f"Hi {name}")
+    print(f"What's the weather like in {location}?")
 
-# greeting_position(name = 'Alex', location = 'London')
-# greeting_position(location = 'London', name = 'Alex')
+greeting_position(name = 'Alex', location = 'London')
+greeting_position(location = 'London', name = 'Alex')
 
-# # Greeting IV
-# def greeting_input(name, location):
-#     print(f"Hi {name}")
-#     print(f"What's the weather like in {location}?")
+# Greeting IV
+def greeting_input(name, location):
+    print(f"Hi {name}")
+    print(f"What's the weather like in {location}?")
 
-# n = input('Name: ')
-# l = input('Location: ')
+n = input('Name: ')
+l = input('Location: ')
 
-# greeting_input(name = n, location = l)
+greeting_input(name = n, location = l)
 
-# # Auditorium LESSON 20 DAY 8 - PAINT AREA CALCULATOR
-# import math
-# def paint_calc(height, width, cover):
-#   number_of_cans = math.ceil((height * width)/cover)
-#   print(f"You'll need {number_of_cans} cans of paint.")
+# Auditorium LESSON 20 DAY 8 - PAINT AREA CALCULATOR
+import math
+def paint_calc(height, width, cover):
+  number_of_cans = math.ceil((height * width)/cover)
+  print(f"You'll need {number_of_cans} cans of paint.")
 
-# h = int(input('Height of the wall: '))
-# w = int(input('Width of the wall: '))
-# coverage = 5
+h = int(input('Height of the wall: '))
+w = int(input('Width of the wall: '))
+coverage = 5
 
-# paint_calc(height = h, width = w, cover=coverage)
+paint_calc(height = h, width = w, cover=coverage)
 
 # Prime number
 def prime_checker(number):
@@ -76,4 +76,6 @@ def prime_checker(number):
 
 n = int(input('Enter a number: '))
 prime_checker(number = n)
+
+
 
--- TestingCode/100_tests.py
+++ TestingCode/100_tests.py
@@ -1,7 +1,34 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+shift = 9
+alphabet_shifted = alphabet[shift:] + alphabet[:shift]
 
-        
+text = 'zulu'
+cipher_text = ''
+
+for i in text:
+    k = alphabet.index(i) #using letter to get index in alphabet list
+    cipher_text += alphabet_shifted[k]
+    #print(f"{i} on  alphabet index is {k}\t|\tvalue of index {k} on alphabet_shifted is {alphabet_shifted[k]}")
+
+
+
+print(f"The encoded text is {cipher_text}")
 
 
 
 
+'''
+print(alphabet)
+print(alphabet_shifted)
 
+
+['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+['d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c']
+
+
+
+# for i, elem in enumerate(text):
+#     k = alphabet.index(elem)
+#     cipher_text += alphabet_shifted[k]
+
+'''
