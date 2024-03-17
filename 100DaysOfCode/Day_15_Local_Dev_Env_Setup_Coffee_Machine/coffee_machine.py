bc61301c36a96334883fed67bc872e7aed09c75c
renekerr <rrenekerr@gmail.com>
2024-03-17 22:33:21 +0100

    coffee machine program 1

---
+++ 100DaysOfCode/Day_15_Local_Dev_Env_Setup_Coffee_Machine/coffee_machine.py
@@ -0,0 +1,12 @@
+# TODO Copy MENU and resources on a different file and import them
+# TODO Ask user to select a type of coffee
+# TODO For maintenance select 'off' TODO To print a report of resources select 'report'
+# TODO Check if there are enough resources to make coffee selected
+# TODO If there are enough resources, then ask user to insert coins
+# TODO Once user has entered all coins, check if it's enough to pay coffee selected
+# TODO If it's not enough, program should warn user that there's not enough money and that it'll be refunded
+
+
+
+
+
