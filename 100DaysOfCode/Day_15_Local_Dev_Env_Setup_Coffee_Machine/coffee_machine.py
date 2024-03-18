a7fbcf88470a2568734ffa80e7024c07ea9943f1
renekerr <rrenekerr@gmail.com>
2024-03-18 20:33:04 +0100

    coffe machine updates

--- 100DaysOfCode/Day_15_Local_Dev_Env_Setup_Coffee_Machine/coffee_machine.py
+++ 100DaysOfCode/Day_15_Local_Dev_Env_Setup_Coffee_Machine/coffee_machine.py
@@ -7,13 +7,6 @@
 # TODO If there's not enough money, program should warn the user that there are insufficient funds and that it'll be refunded.
 # TODO If there are enough resources and money, deliver the coffee.
 # TODO The program should stop only when there is not enough resources, money, or the word "off" is selected.
-
-"""
-Prices:
-    Espresso    : 1.50
-    Latte       : 2.50
-    Cappuccino  : 3.00
-"""
 from menu_resources import *
 
 
@@ -22,24 +15,59 @@ def report():
     milk_available = resources['milk']
     coffee_available = resources['coffee']
 
+    print('\nSummary')
     print(f'Water   : {water_available} ml')
     print(f'Milk    : {milk_available} ml')
     print(f'Coffee  : {coffee_available} gr')
-    print(f'Money   : {money} $')
+    print(f'Money   : {money} $\n')
+
+
+def resources_check(user_choice):
+    water_needed = MENU[user_choice]["ingredients"]["water"]
+
+    if user_choice == 'espresso':
+        milk_needed = 0
+    else:
+        milk_needed = MENU[user_choice]["ingredients"]["milk"]
+
+    coffee_needed = MENU[user_choice]["ingredients"]["coffee"]
+
+    if resources['water'] < water_needed:
+        print('Sorry there is not enough water.')
+    elif resources['milk'] < milk_needed:
+        print('Sorry there is not enough milk.')
+    elif resources['coffee'] < coffee_needed:
+        print('Sorry there is not enough coffee.')
+    else:
+        print('Resources OK')
 
 
 money = 0
-# user_choice = input('What would you like? (espresso/latte/cappuccino): ').lower()
-user_choice = 'latte'
-print(f'Your selection: {user_choice.title()}')
-print(f"Price: {MENU[user_choice]['cost']:.2F} $")
+quarters = 0.25
+dimes = 0.10
+nickles = 0.05
+pennies = 0.01
 
-# Compare selection against resources
-for k, v in MENU[user_choice]['ingredients'].items():
-    print(k, v)
 
-for i, j in resources.items():
-    print(i, j)
+machine_off = False
+while not machine_off:
+
+    user_choice = input('What would you like? (espresso/latte/cappuccino): ').lower()
+    if user_choice == 'off':
+        print('Coffee machine turned off')
+        machine_off = True
+    elif user_choice == 'report':
+        report()
+    elif user_choice in ('espresso', 'latte', 'cappuccino'):
+        resources_check(user_choice)
+        print(f'\nYour selection: {user_choice.title()}')
+        print(f"Price: {MENU[user_choice]['cost']:.2F} $\n")
+        print('Please insert coins')
+        quarters_amount = int(input('How many quarters? '))
+        dimes_amount = int(input('How many dimes? '))
+        nickles_amount = int(input('How many nickles? '))
+        pennies_amount = int(input('How many pennies? '))
+
 
 
 
