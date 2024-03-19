9e2526aa006d59779d25c1181ff419c6dad808a1
renekerr <rrenekerr@gmail.com>
2024-03-19 22:10:18 +0100

    coffe machine updates

--- 100DaysOfCode/Day_15_Local_Dev_Env_Setup_Coffee_Machine/coffee_machine_imprv.py
+++ 100DaysOfCode/Day_15_Local_Dev_Env_Setup_Coffee_Machine/coffee_machine_imprv.py
@@ -1,7 +1,11 @@
 from menu_resources import *
 
+# Constants
+COIN_VALUES = {'quarters': 0.25, 'dimes': 0.10, 'nickels': 0.05, 'pennies': 0.01}
 
-def resources_update(water, milk, coffee):
+
+def update_resources(water, milk, coffee):
+    """Update the resources after serving a drink."""
     resources['water'] -= water
     resources['milk'] -= milk
     resources['coffee'] -= coffee
@@ -9,6 +13,7 @@ def resources_update(water, milk, coffee):
 
 
 def print_report(curr_w, curr_m, curr_c, curr_cash):
+    """Print the current resources report."""
     print('\nSummary')
     print(f'\tWater   : {curr_w} ml')
     print(f'\tMilk    : {curr_m} ml')
@@ -16,33 +21,21 @@ def print_report(curr_w, curr_m, curr_c, curr_cash):
     print(f'\tMoney   : {curr_cash} $')
 
 
-def resources_check(user_choice):
+def check_resources(user_choice):
+    """Check if there are enough resources to make the selected drink."""
     resources_output = True
+
     water_needed = MENU[user_choice]["ingredients"]["water"]
-
-    if user_choice == 'espresso':
-        milk_needed = 0
-    else:
-        milk_needed = MENU[user_choice]["ingredients"]["milk"]
-
+    milk_needed = MENU[user_choice]["ingredients"].get("milk", 0)  # Default to 0 if milk is not specified (if
+    # user_selection equal to espresso
     coffee_needed = MENU[user_choice]["ingredients"]["coffee"]
 
-    if resources['water'] < water_needed:
-        print('Sorry there is not enough water.')
-        resources_output = False
-        return resources_output
-    elif resources['milk'] < milk_needed:
-        print('Sorry there is not enough milk.')
-        resources_output = False
-        return resources_output
-    elif resources['coffee'] < coffee_needed:
-        print('Sorry there is not enough coffee.')
-        resources_output = False
-        return resources_output
-    else:
-        print('Resources OK')
-        resources_output = True
-        return resources_output
+    for ingredient, value in MENU[user_selection]["ingredients"].items():
+        if resources[ingredient] < value:
+            print(f'Sorry, there is not enough {ingredient}.')
+            resources_output = False
+        else:
+            return resources_output
 
 
 def money_provided_sum(quarters_in, dimes_in, nickles_in, pennies_in):
@@ -78,10 +71,10 @@ while not machine_off:
         print('Coffee machine has been turned OFF.')
         machine_off = True
     elif user_selection == 'report':
-        w, m, c = resources_update(water=water_used, milk=milk_used, coffee=coffee_used)
+        w, m, c = update_resources(water=water_used, milk=milk_used, coffee=coffee_used)
         print_report(curr_w=w, curr_m=m, curr_c=c, curr_cash=cash)
     elif user_selection in ('espresso', 'latte', 'cappuccino'):
-        resource_flag = resources_check(user_choice=user_selection)
+        resource_flag = check_resources(user_choice=user_selection)
 
         if resource_flag:
             coffee_cost = MENU[user_selection]['cost']
@@ -95,10 +88,10 @@ while not machine_off:
             pennies_entered = int(input('\tHow many pennies? '))
             total_cash_entered = money_provided_sum(quarters_in=quarters_entered, dimes_in=dimes_entered, nickles_in=nickles_entered, pennies_in=pennies_entered)
 
-            trans_success = transaction_validation(total_cash=total_cash_entered, cost=coffee_cost, selection=user_selection)
+            transaction_success = transaction_validation(total_cash=total_cash_entered, cost=coffee_cost, selection=user_selection)
 
-            if trans_success:
-                print('Resources updating...please wait!')
+            if transaction_success:
+                # Resources update
                 cash += coffee_cost
                 water_used = MENU[user_selection]["ingredients"]["water"]
 
